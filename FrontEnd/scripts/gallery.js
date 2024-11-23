7a581a7394042a596247e761e658a0ba26574fa3
Kevin BERTRAND <kevinbertrand68.pro@gmail.com>
2024-11-23 03:36:49 +0100

    Ajout des filtres

--- FrontEnd/scripts/gallery.js
+++ FrontEnd/scripts/gallery.js
@@ -1,8 +1,8 @@
-function showGallery(tableau) {
+function showGallery(array) {
 
     document.querySelector('.gallery').innerHTML= ""; // Efface la gallerie
 
-    tableau.forEach(element => {
+    array.forEach(element => {
         const figureElement = document.createElement('figure');
         const imageElement = document.createElement('img');
         const captionElement = document.createElement('figcaption');
@@ -24,9 +24,20 @@ function generateFilters() {
         <button class="filterButton" id="Tous">Tous</button>
         <button class="filterButton" id="Objets">Objets</button>
         <button class="filterButton" id="Appartements">Appartements</button>
-        <button class="filterButton" id="Hotel">Hôtels & restaurants</button>
+        <button class="filterButton" id="Hotel">Hotels & restaurants</button>
     `;
 
     parent.insertAdjacentHTML('beforeend', filtersHTML);
 }
 
+function filter(filter, data,categories) {
+    let id = 0;
+    categories.forEach(element => {
+        if(filter === element.name){
+            id = element.id;
+        }
+    });
+    const filteredData = data.filter(data => data.categoryId === id);
+    
+    showGallery(id === 0 ? data : filteredData);
+}
--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,13 +1,32 @@
-
+let arrayTest = "";
+let categories = "";
 
 fetch('http://localhost:5678/api/works')
     .then(response => response.json()) // Convertir la réponse en JSON
     .then(data => {
         showGallery(data); // Appeler la fonction avec les données résolues
+        arrayTest = data
+
     })
     .catch(error => {
         console.error('Erreur lors de la récupération des données :', error);
     });
 
-    generateFilters()
-    
+fetch('http://localhost:5678/api/categories')
+    .then(response => response.json())
+    .then(data => {
+        categories = data;
+    })
+
+
+
+generateFilters();
+
+const buttons = document.querySelectorAll('.filterButton');
+
+buttons.forEach(button => {
+    button.addEventListener('click', event => {
+        const button = event.target.innerText;
+        filter(button,arrayTest,categories);
+    });
+});
