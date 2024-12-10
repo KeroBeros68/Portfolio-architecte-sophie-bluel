bb9e6033ba5bf7b1b1e898cc1bf5daed03a27a71
Kevin BERTRAND <kevinbertrand68.pro@gmail.com>
2024-12-10 05:28:14 +0100

    Amelioration 1ere fenetre modale

--- FrontEnd/scripts/modale.js
+++ FrontEnd/scripts/modale.js
@@ -1,30 +1,29 @@
 function showModale() {
-    const modale = `
-        <aside id="modale" class="modale" role="dialog" aria-modal="false" aria-labelledby="modaleTitle">
-            <div class="modale-Wrapper">
-                <div>
-                    <button id="close-Modale"><i class="fa-solid fa-xmark fa-xl"></i></button>
-                </div>
-                <h2 id="modaleTitle">Galerie photo</h2>
-                <div class='main-Modale'>
-                    <div class="modale-Gallery">
-                    </div>
-                </div>
-                <input type="button" value="Ajouter une photo"></input>
-            </div>
-        </aside>
-        `;
-
-    document.body.insertAdjacentHTML('beforeend', modale);
-
-    showModaleGallery();
+    document.querySelector('#modale').removeAttribute('style');
+    ModaleGallery();
     document.querySelector('#modale').addEventListener('click', closeModale);
     document.querySelector('#close-Modale').addEventListener('click', closeModale);
-    document.querySelector('.modale-Wrapper').addEventListener('click', stopPropagation);
+    document.querySelector('#modale-Wrapper').addEventListener('click', function stopPropagation(e) {
+        e.stopPropagation();
+    });
 }
 
-function showModaleGallery() {
-    const gallery = JSON.parse(window.localStorage.getItem("gallery"));
+function closeModale() {
+    document.querySelector('#modale').setAttribute('style','display:none');
+    document.querySelector('.modale-Gallery').innerHTML = ''; 
+    document.querySelector('#modale').removeEventListener('click', closeModale);
+    document.querySelector('#close-Modale').removeEventListener('click', closeModale);
+}
+
+
+function ModaleGallery() {
+    const gallery = JSON.parse(window.localStorage.getItem('gallery'));
+    const galleryContainer = document.createElement('div');
+    galleryContainer.className = 'modale-Gallery';
+
+    const inputGallery = document.createElement('div');
+    inputGallery.className = 'input-Modale';
+    inputGallery.innerHTML = '<input type="button" value="Ajouter une photo"></input>';
 
     gallery.forEach(element => {
         const articleElement = document.createElement('article');
@@ -35,15 +34,11 @@ function showModaleGallery() {
         articleElement.appendChild(imageElement);
         articleElement.innerHTML += trashElement;
 
-        document.querySelector('.modale-Gallery').appendChild(articleElement);
+        galleryContainer.appendChild(articleElement);
     })
+
+    document.querySelector('.main-Modale').appendChild(galleryContainer);
+    document.querySelector('.main-Modale').appendChild(inputGallery);
 }
 
-function closeModale() {
-        const aside = document.getElementById('modale');
-        aside.remove();
-}
 
-function stopPropagation(e) {
-    e.stopPropagation();
-}
